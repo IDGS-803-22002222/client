7a281461b905b524485b2e5106da02b3af82fdfc
IDGS-903-22002222 <78643@alumnos.utleon.edu.mx>
2025-07-31 18:56:26 -0600

    actualizacion

---
+++ src/app/pages/home/home.ts
@@ -0,0 +1,15 @@
+import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Auth } from '../../services/auth';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './home.html',
+  styleUrl: './home.css'
+})
+export class Home {
+  auth = inject(Auth);
+
+}
---
+++ src/app/pages/login/login.ts
@@ -0,0 +1,58 @@
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { Router, RouterLink } from '@angular/router';
+import { Auth } from '../../services/auth';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+@Component({
+  selector: 'app-login',
+standalone: true,
+  imports: [
+    MatInputModule,
+    RouterLink,
+    MatSnackBarModule,
+    MatIconModule,
+    ReactiveFormsModule,
+  ],
+  templateUrl: './login.html',
+  styleUrl: './login.css'
+})
+export class Login {
+authService = inject(Auth);
+  matSnackBar = inject(MatSnackBar);
+  router = inject(Router);
+  hide = true;
+  form!: FormGroup;
+  fb = inject(FormBuilder);
+
+  login() {
+    this.authService.login(this.form.value).subscribe({
+      next: (response) => {
+        this.matSnackBar.open(response.message, 'Close', {
+          duration: 5000,
+          horizontalPosition: 'center',
+        });
+        this.router.navigate(['/']);
+      },
+      error: (error) => {
+        this.matSnackBar.open(error.error.message, 'Close', {
+          duration: 5000,
+          horizontalPosition: 'center',
+        });
+      },
+    });
+  }
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+  }
+}
